1a0d27936d183b92888740fcb9b5295f5f407dc3
mofrim <mofrim@posteo.de>
2025-05-03 20:28:34 +0200

    goedb: add nix files

---
+++ shell.nix
@@ -0,0 +1,21 @@
+{ pkgs ? import <nixpkgs> {} }:
+#let
+  #pkgs = import (builtins.fetchGit {
+    #url = "/home/frido/nix/nixpkgs-fork/";
+    #ref = "master";
+    # ref = "nixos-24.05";
+  #}) {};
+#in
+pkgs.mkShell {
+  name = "bla-env";
+  buildInputs = with pkgs; [
+    (python311.withPackages(ps: with ps; [
+      flask
+    ]))
+    nodejs_22
+    pipenv
+  ];
+  shellHook = ''
+    echo "hi there!"
+  '';
+}
